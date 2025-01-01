5c435fd1417eaaef953d70c47a19150956690fa2
aditya2410bhati <aditya2410bhati@gmail.com>
2025-01-01 10:33:34 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyA4YrlviLql-wa5RpCGqfLCx4OkssFqux4',
+      authDomain: 'sample-firebase-ai-apps-16862.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-apps-16862',
+      storageBucket: 'sample-firebase-ai-apps-16862.firebasestorage.app',
+      messagingSenderId: '804555550617',
+      appId: '1:804555550617:web:f3d972105d7ac9711ced08',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyC2Ars2E_6TCTyv35lh46RhGMWjpeC7dkI',
+  debug_token: ''
+};
